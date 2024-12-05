2b865f2a9451e1fc5e9b3db79a0dc313481789d6
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-12-05 12:05:07 -0300

    feat: implement route path builder

---
+++ src/utils/build-route-path.ts
@@ -0,0 +1,11 @@
+export function buildRoutePath(path: string) {
+  const routeParamRegex = /:([a-zA-Z]+)/g;
+  const pathWithParams = path.replaceAll(
+    routeParamRegex,
+    "(?<$1>[a-z0-9\\-_]+)"
+  );
+
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
+
+  return pathRegex;
+}
