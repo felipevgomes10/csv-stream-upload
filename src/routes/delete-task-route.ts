413c88cf1c2d58e28aa8fd718a1d50b4e255e591
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-12-13 17:16:04 -0300

    feat: implement task delete route

--- src/routes/delete-task-route.ts
+++ src/routes/delete-task-route.ts
@@ -1,10 +1,25 @@
+import { database } from "@/infra/database/database";
 import { buildRoutePath } from "@/utils/build-route-path";
 import type { Route } from "./routes";
 
 export const deleteTaskRoute: Route = {
   method: "DELETE",
   path: buildRoutePath("/tasks/:id"),
-  handler: async (_req, res) => {
-    res.end(JSON.stringify({ message: "Delete task" }));
+  handler: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const [task] = await database.select("tasks", { id });
+
+      if (!task) {
+        res.writeHead(404).end(JSON.stringify({ message: "Task not found" }));
+        return;
+      }
+
+      await database.delete("tasks", { id });
+
+      res.writeHead(204).end();
+    } catch (error) {
+      res.writeHead(500).end(JSON.stringify({ error }));
+    }
   },
 };
