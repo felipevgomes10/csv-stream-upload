252ec38ae5fdce7e7a3a0cb67030a9cdd2c3a7d9
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-12-13 18:28:55 -0300

    feat: decode URI string

--- src/utils/extract-query-params.ts
+++ src/utils/extract-query-params.ts
@@ -1,12 +1,10 @@
-import type { QueryParams } from "@/routes/routes";
-
 export function extractQueryParams(query: string): QueryParams {
   const queryParams = query.substring(1).split("&");
 
   const params = queryParams.reduce((acc, queryParam) => {
     const [key, value] = queryParam.split("=");
 
-    if (key && value) acc[key as keyof QueryParams] = value;
+    if (key && value) acc[key as keyof QueryParams] = decodeURIComponent(value);
 
     return acc;
   }, {} as QueryParams);
