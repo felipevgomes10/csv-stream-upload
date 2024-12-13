dacd5765cc13b6b35283550f86d1b772dcaf28bd
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-12-13 16:31:04 -0300

    feat: implement task post route

--- src/routes/create-task-route.ts
+++ src/routes/create-task-route.ts
@@ -1,10 +1,44 @@
+import { Task } from "@/application/entity/task/task-entity";
+import { database } from "@/infra/database/database";
 import { buildRoutePath } from "@/utils/build-route-path";
+import { z } from "zod";
 import type { Route } from "./routes";
+
+const createTaskSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+});
 
 export const createTaskRoute: Route = {
   method: "POST",
   path: buildRoutePath("/tasks"),
-  handler: async (_req, res) => {
-    res.end(JSON.stringify({ message: "Create task" }));
+  handler: async (req, res) => {
+    try {
+      const { error, success, data } = createTaskSchema.safeParse(req.body);
+
+      if (!success) {
+        const { fieldErrors } = error.flatten();
+        const errorMessage = !req.body
+          ? "title and description are required"
+          : fieldErrors;
+
+        res.writeHead(400).end(JSON.stringify({ error: errorMessage }));
+        return;
+      }
+
+      const { title, description } = data;
+      const task = new Task({ title, description });
+
+      await database.insert("tasks", task);
+
+      res.writeHead(201).end(
+        JSON.stringify({
+          message: "Task created successfully",
+          task: { id: task.id },
+        })
+      );
+    } catch (error) {
+      res.writeHead(500).end(JSON.stringify({ error }));
+    }
   },
 };
