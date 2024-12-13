a77d83dcc3254133fa4e257f7720b9ff4b119d73
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-12-13 17:15:22 -0300

    feat: implement task put route

--- src/routes/update-task-route.ts
+++ src/routes/update-task-route.ts
@@ -1,10 +1,56 @@
+import { Task } from "@/application/entity/task/task-entity";
+import { database } from "@/infra/database/database";
 import { buildRoutePath } from "@/utils/build-route-path";
+import z from "zod";
 import type { Route } from "./routes";
+
+const updateTaskSchema = z.object({
+  title: z.string().optional(),
+  description: z.string().optional(),
+  completed: z.boolean().optional(),
+});
 
 export const updateTaskRoute: Route = {
   method: "PUT",
   path: buildRoutePath("/tasks/:id"),
-  handler: async (_req, res) => {
-    res.end(JSON.stringify({ message: "Update task" }));
+  handler: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const [task] = await database.select("tasks", { id });
+
+      if (!task) {
+        res.writeHead(404).end(JSON.stringify({ message: "Task not found" }));
+        return;
+      }
+
+      const { error, success, data } = updateTaskSchema.safeParse(req.body);
+
+      if (!success) {
+        const { fieldErrors } = error.flatten();
+
+        res.writeHead(400).end(JSON.stringify({ error: fieldErrors }));
+        return;
+      }
+
+      const { title, description, completed } = data;
+      const updatedTask = new Task(task);
+
+      updatedTask.title = title ?? updatedTask.title;
+      updatedTask.description = description ?? updatedTask.description;
+
+      if (completed) updatedTask.complete();
+      if (completed === false) updatedTask.uncomplete();
+
+      await database.update("tasks", updatedTask.toJSON(), { id });
+
+      res.writeHead(200).end(
+        JSON.stringify({
+          message: "Task updated successfully",
+          task: updatedTask,
+        })
+      );
+    } catch (error) {
+      res.writeHead(500).end(JSON.stringify({ error }));
+    }
   },
 };
