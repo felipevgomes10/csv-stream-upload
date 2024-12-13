5d4e4d8d6ec0f2041747043a063aec4e14e1b063
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-12-13 16:31:25 -0300

    feat: implement task get by id route

--- src/routes/get-task-route.ts
+++ src/routes/get-task-route.ts
@@ -1,10 +1,23 @@
+import { database } from "@/infra/database/database";
 import { buildRoutePath } from "@/utils/build-route-path";
 import type { Route } from "./routes";
 
 export const getTaskRoute: Route = {
   method: "GET",
   path: buildRoutePath("/tasks/:id"),
-  handler: async (_req, res) => {
-    res.end(JSON.stringify({ message: "Task" }));
+  handler: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const [task] = await database.select("tasks", { id });
+
+      if (!task) {
+        res.writeHead(404).end(JSON.stringify({ message: "Task not found" }));
+        return;
+      }
+
+      res.writeHead(200).end(JSON.stringify({ task }));
+    } catch (error) {
+      res.writeHead(500).end(JSON.stringify({ error }));
+    }
   },
 };
